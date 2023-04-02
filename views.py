7cb3cfeee385589b3999f546766981198ec58d49
Gaston Donikian <gdonikian@itba.edu.ar>
2023-04-02 04:11:14 -0300

    added html

--- views.py
+++ views.py
@@ -6,12 +6,13 @@ views = Blueprint(__name__, "views")
 
 import json
 
-def numpy_to_json(arr):
-    # Define column names
-    column_names = ["cancerType","patientID","age","sex","ethnicity",
+column_names = ["cancerType","patientID","age","sex","ethnicity",
                     "mentalHealth","generalHealth","tumorSize","cancerStage",
                     "surgicalMethod","radiationMethod","chemotherapy",
                     "effectivenessOfTreatment","descripcion"]
+
+def numpy_to_json(arr):
+    
     # Convert numpy array to list of dictionaries
     data = [dict(zip(column_names, row)) for row in arr]
     # Convert to JSON string
@@ -35,12 +36,11 @@ def getValue():
     #llamado a la funcion con los pacientes cercanos
 
     # Example usage of the function
-    # input_point = {'age':age, 'sex':sex,'ethnicity':ethnicity,'mentalHealth':mentalHealth,'generalHealth':generalHealth,'tumorSize':tumorSize,'cancerType':cancerType,'cancerStage':cancerStage}
-    input_point = {'age':70, 'sex':0,'ethnicity':1,'mentalHealth':4,'generalHealth':10,'tumorSize':10,'cancerType':0,'cancerStage':1}
+    input_point = {'age':int(age), 'sex':int(sex),'ethnicity':int(ethnicity),'mentalHealth':int(mentalHealth),'generalHealth':int(generalHealth),'tumorSize':float(tumorSize),'cancerType':int(cancerType),'cancerStage':int(cancerStage)}
+    #input_point = {'age':70, 'sex':0,'ethnicity':1,'mentalHealth':4,'generalHealth':10,'tumorSize':10,'cancerType':0,'cancerStage':1}
     answer = get_nearest_points(input_point)
     #ponele que answer es json con cada caso + porcentaje de similaridad
-    print(numpy_to_json(answer)[0])
-    return render_template('index.html',data=numpy_to_json(answer))
+    return render_template('answer.html',data=numpy_to_json(answer), columnNames = column_names)
 
 
 @views.route("/json")
