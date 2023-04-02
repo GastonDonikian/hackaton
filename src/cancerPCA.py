36c7030250548df0c115cb062e6d3777be39bfd7
Gaston Donikian <gdonikian@itba.edu.ar>
2023-04-02 03:38:23 -0300

    Connected to frontend

--- src/cancerPCA.py
+++ src/cancerPCA.py
@@ -4,59 +4,56 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from scipy.spatial.distance import cdist
 
-# Read data from CSV file
-df = pd.read_csv('dataFilterByUs.csv')
-# Remove the first two columns
-df = df.iloc[:, 2:]
-
-# Remove the last four columns
-df = df.iloc[:, :-5]
-# Separate columns into arrays
-
-# Create dummy variables for the third and fourth columns
-df = pd.get_dummies(df, columns=['sex', 'ethnicity'])
-
-X = df.values
-column_names = df.columns
 
 
-# Standardize the feature matrix
-scaler = StandardScaler()
-X_std = scaler.fit_transform(X)
-print(column_names)
-print(X_std)
-# Perform PCA
-pca = PCA()
-pca.fit(X_std)
 
-def get_trait_weights():
-
-
-    # Print the index of each column in descending order of importance
-    variance_ratios = pca.explained_variance_ratio_
-    sorted_indices = np.argsort(variance_ratios)[::-1]
-    answer = {}
-    for i in sorted_indices:
-        answer[column_names[i]] = variance_ratios[i]
-        # print(f"Column name {column_names[i]} has a variance ratio of {(variance_ratios[i]*100):.2f} %")
-    # print(answer)
-    return answer
 
 
 # Function to return 10 nearest points to a given input
 def get_nearest_points(input_point):
+        # Read data from CSV file
+    df = pd.read_csv('dataFilterByUs.csv')
+    df = df[df['cancerType'] == input_point['cancerType']]
+    all_data = pd.read_csv('dataFilterByUs.csv').values
+    
+    input_point = [input_point['age'],input_point['mentalHealth'],input_point['generalHealth'],input_point['tumorSize'],input_point['cancerStage'],1 if input_point['sex'] == 0 else 0, 1 if input_point['sex'] == 1 else 0,
+                             1 if input_point['ethnicity'] == 0 else 0,1 if input_point['ethnicity'] == 1 else 0,1 if input_point['ethnicity'] == 2 else 0,1 if input_point['ethnicity'] == 3 else 0]
+
+    # Remove the first two columns
+    print(df)
+    df = df.iloc[:, 2:]
+
+    # Remove the last four columns
+    df = df.iloc[:, :-5]
+    # Separate columns into arrays
+
+    # Create dummy variables for the third and fourth columns
+    df = pd.get_dummies(df, columns=['sex', 'ethnicity'])
+
+    X = df.values
+    column_names = df.columns
+    print(column_names)
+
+    # Standardize the feature matrix
+    scaler = StandardScaler()
+    X_std = scaler.fit_transform(X)
+    # Perform PCA
+    pca = PCA()
+    pca.fit(X_std)
+    
     input_std = scaler.transform([input_point])
     input_pca = pca.transform(input_std)
     distances = cdist(input_pca, pca.transform(X_std), 'euclidean')
     nearest_indices = np.argsort(distances)[0][:10]
-    nearest_points = X[nearest_indices]
+    nearest_points = all_data[nearest_indices]
     nearest_distances = distances[0][nearest_indices]
-    return nearest_points, nearest_distances
+    max_distance = np.max(nearest_distances)
+    similarities = (max_distance - nearest_distances) / max_distance * 100
+    nearest_points = np.concatenate((nearest_points, np.array(similarities)[:, np.newaxis]), axis=1)
+    return nearest_points
 
+# # Example usage of the function
+# input_point = {'age':70, 'sex':0,'ethnicity':1,'mentalHealth':4,'generalHealth':10,'tumorSize':10,'cancerType':0,'cancerStage':1}
+# nearest_points = get_nearest_points(input_point)
 
-# Example usage of the function
-# print(get_trait_weights())
-input_point = [70,0,4,10,2,1,0,1, 0,0,0]
-nearest_points, nearest_distances = get_nearest_points(input_point)
-print(f"The 10 nearest points to {input_point} are:\n{nearest_points}")
-print(f"The distances of the 10 nearest points are:\n{nearest_distances}")
+# print(f"The 10 nearest points to {input_point} are:\n{nearest_points}\n")
--- views.py
+++ views.py
@@ -1,7 +1,22 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, url_for
-
-
+from src.cancerPCA import *
+import json
 views = Blueprint(__name__, "views")
+
+
+import json
+
+def numpy_to_json(arr):
+    # Define column names
+    column_names = ["cancerType","patientID","age","sex","ethnicity",
+                    "mentalHealth","generalHealth","tumorSize","cancerStage",
+                    "surgicalMethod","radiationMethod","chemotherapy",
+                    "effectivenessOfTreatment","descripcion"]
+    # Convert numpy array to list of dictionaries
+    data = [dict(zip(column_names, row)) for row in arr]
+    # Convert to JSON string
+    json_string = json.dumps(data)
+    return data
 
 @views.route("/")
 def home():
@@ -16,8 +31,16 @@ def getValue():
     generalHealth = request.form.get("generalHealth")
     cancerType = request.form.get("cancerType")
     cancerStage = request.form.get("cancerStage")
+    tumorSize = request.form.get("tumorSize")
     #llamado a la funcion con los pacientes cercanos
-    return render_template('index.html', age = age, sex = sex, ethnicity = ethnicity,mentalHealth = mentalHealth,generalHealth = generalHealth,cancerType = cancerType, cancerStage = cancerStage)
+
+    # Example usage of the function
+    # input_point = {'age':age, 'sex':sex,'ethnicity':ethnicity,'mentalHealth':mentalHealth,'generalHealth':generalHealth,'tumorSize':tumorSize,'cancerType':cancerType,'cancerStage':cancerStage}
+    input_point = {'age':70, 'sex':0,'ethnicity':1,'mentalHealth':4,'generalHealth':10,'tumorSize':10,'cancerType':0,'cancerStage':1}
+    answer = get_nearest_points(input_point)
+    #ponele que answer es json con cada caso + porcentaje de similaridad
+    print(numpy_to_json(answer)[0])
+    return render_template('index.html',data=numpy_to_json(answer))
 
 
 @views.route("/json")
